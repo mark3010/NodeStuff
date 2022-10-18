fd17e2aa82690efe65cb9b6144e5d301404fd717
mark3010 <marklarsen96@gmail.com>
2022-10-18 17:09:13 +0200

    refactored folders, fixed typos

--- myExcercises/time_to_die/app.js
+++ myExcercises/time_to_die/app.js
@@ -2,15 +2,20 @@ import express from "express"; //imports from node modules
 import path from "path";
 
 const app = express();
+const router = express.Router();
 
 app.listen(8080, () => console.log("the server is running"));
 
 //let frontend get public folder
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use('/.netlify/functions/api',router);
 
+router.get('/',(req,res) => {
+    res.sendFile(path.resolve("./public/frontpage.html"));
+})
 
+//app.get("/", (req, res) =>{
+    //res.sendFile(__dirname + "/public/frontpage.html");  
+//});
 
-app.get("/", (req, res) =>{
-    //res.sendFile(__dirname + "/public/frontpage.html");
-    res.sendFile(path.resolve("./public/frontpage.html"))
-});
+module.exports.handler = serverless(app);
